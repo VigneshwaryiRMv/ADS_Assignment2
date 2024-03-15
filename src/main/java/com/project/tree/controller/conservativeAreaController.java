1e04d6aa06a162bba208da958da2ca962ed68460
Shanmuhapriya <68014808+Shanmuhapriya@users.noreply.github.com>
2024-03-15 19:29:39 +0000

    Controller test case

--- src/main/java/com/project/tree/controller/conservativeAreaController.java
+++ src/main/java/com/project/tree/controller/conservativeAreaController.java
@@ -14,8 +14,7 @@ import java.util.List;
 
 @RestController
 public class conservativeAreaController {
-    @Autowired
-    private conservationSystem conservationSystemService;
+    @Autowired conservationSystem conservationSystemService;
 
     @PostMapping("/area")
     public ResponseEntity<String> addConservationArea(@RequestBody ConservationAreaRequest conservationAreaRequest) {
